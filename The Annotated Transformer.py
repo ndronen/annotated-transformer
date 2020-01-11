20ad0d77fa9b9c9b782668372b804a1429da4e3e
Nicholas Dronen <ndronen@gmail.com>
2020-01-11 01:03:21 -0700

    Refactor EncoderDecoder to enable printing of state shapes.

--- The Annotated Transformer.py
+++ The Annotated Transformer.py
@@ -45,13 +45,17 @@ class EncoderDecoder(nn.Module):
 
     def forward(self, src, tgt, src_mask, tgt_mask):
         "Take in and process masked src and target sequences."
-        return self.decode(self.encode(src, src_mask), src_mask, tgt, tgt_mask)
+        encoding = self.encode(src, src_mask)
+        decoding = self.decode(encoding, src_mask, tgt, tgt_mask)
+        return decoding
 
     def encode(self, src, src_mask):
-        return self.encoder(self.src_embed(src), src_mask)
+        embedding = self.src_embed(src)
+        encoding = self.encoder(embedding, src_mask)
 
     def decode(self, memory, src_mask, tgt, tgt_mask):
-        return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
+        embedding = self.tgt_embed(tgt)
+        decoding = self.decoder(embedding, memory, src_mask, tgt_mask)
 
 
 class Generator(nn.Module):
